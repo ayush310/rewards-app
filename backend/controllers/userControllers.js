542c134a92b3f867875693dee74559a05ef22567
ayush310 <74891288+ayush310@users.noreply.github.com>
2023-07-20 11:18:25 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,7 +39,7 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (user && user.matchPassword(password)) {
+  if (user && (await user.matchPassword(password))) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
